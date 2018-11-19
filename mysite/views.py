76db6ad560029cf4ab419c88cdfb26573c0b4f24
Amir Dezyanian <a.dezyanian@mabnatelecom.com>
2018-11-19 11:55:35 +0330

    initial commit

---
+++ mysite/views.py
@@ -0,0 +1,15 @@
+from django.http import Http404, HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("This is the first webserver with django")
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
